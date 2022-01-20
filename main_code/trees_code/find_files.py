9bd5a5155f5432982068913a1b0e9df1d48a6003
vova <vladimir1012kov@gmail.com>
2022-01-20 10:22:53 +0300

    added file find_files.py

---
+++ main_code/trees_code/find_files.py
@@ -0,0 +1,21 @@
+from hexlet.fs import is_directory, is_file, mkfile, mkdir, get_name, get_children
+
+
+tree = mkdir('/', [
+    mkdir('etc', [
+        mkdir('apache'),
+        mkdir('nginx', [
+            mkfile('nginx.conf', {'size': 800}),
+        ]),
+        mkdir('consul', [
+            mkfile('config.json'),
+            mkfile('data'),
+            mkfile('raft'),
+        ]),
+    ]),
+    mkfile('hosts', {'size': 3500}),
+    mkfile('resolve', {'size': 1000}),
+])
+
+def find_files_by_name(path, name):
+    path
